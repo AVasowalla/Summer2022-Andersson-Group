47647ff10458a18e81d4177166b289aa7349bc81
AVasowalla <108306331+AVasowalla@users.noreply.github.com>
2022-07-28 15:09:48 -0400

    Update plot_rates.py

--- wireless_com/scripts/plot_rates.py
+++ wireless_com/scripts/plot_rates.py
@@ -16,12 +16,11 @@ ax = fig.add_subplot(1, 1, 1)
 times = {}
 color_list = []
 rates = {}
-num_sens = 3
+num_sens = 3 #change this to the number of sensors you are using
 i = 0
 
 #read transmission rates from rostopic and store in a global variable also creates a list of colors based on the number of sensors
 def callback(data):
-
 	global color_list
 	global rates
 	global times
@@ -30,9 +29,7 @@ def callback(data):
 	color_map = cm.get_cmap('nipy_spectral')
 
 	if data.name != 0:
-
 		if data.name not in rates.keys():
-
 			rates[data.name] = []
 			times[data.name] = []
 
@@ -44,26 +41,25 @@ def callback(data):
 			temp_rate = [0] * len(temp_time)
 			rates[data.name].pop()
 			times[data.name].pop()
+
 			for x in range(len(temp_time)):
 				rates[data.name].append(temp_rate[x])
 				times[data.name].append(temp_time[x])
+
 			rates[data.name].append(data.rate)
 			times[data.name].append(data.time)
 
 	while i < num_sens:
-
 		color_list.append(color_map((1.0/num_sens)*i))
 		i += 1
 
 #standard ros subscriber
 def listener():
-
 	rospy.init_node("rate_plotter")
 	rospy.Subscriber("sensor_rates", transfer_rate, callback, queue_size=1)
 
 #animated plotting function, also filters noise from the transmission rates
 def animate(i):
-
 	ax.clear()
 
 	n = 1000
@@ -71,22 +67,17 @@ def animate(i):
 	a = 1
 
 	if len(rates.keys()) > 0:
-
 		for key in rates.keys():
-
 			filtered = lfilter(b, a, rates[key])
 
 			while len(filtered) < len(times[key]):
-
 				filtered = np.append(filtered, filtered[-1])
 
 			try:
-
 				ax.plot(times[key], filtered, color=color_list[key-1])
 				reset = True
 
 			except Exception:
-
 				pass
 
 		plt.title("Data Transmission Rates vs Time")
@@ -95,7 +86,6 @@ def animate(i):
 		plt.legend(rates.keys(), loc='upper left')
 
 if __name__ == '__main__':
-
 	listener()
 	ani = animation.FuncAnimation(fig, animate, interval=100)
 	plt.show()
