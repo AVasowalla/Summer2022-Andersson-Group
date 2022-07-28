e45acad78569960438ad6267e3b5df1d07b368e5
AVasowalla <108306331+AVasowalla@users.noreply.github.com>
2022-07-28 15:06:39 -0400

    Update integrate_rates.py

--- wireless_com/scripts/integrate_rates.py
+++ wireless_com/scripts/integrate_rates.py
@@ -14,14 +14,12 @@ integral = sent_data()
 
 #read transmission rates from rostopic, filter the noise, calculate the integral, then store it in a global variable
 def callback(data):
-	
 	global rates
 	global times
 	global integral
 	global sens_data
 
 	if data.name not in rates.keys():
-
 		rates[data.name] = []
 		times[data.name] = []
 		sens_data[data.name] = 0
@@ -40,10 +38,11 @@ def callback(data):
 	filtered = lfilter(b, a, rates[data.name])
 
 	while len(filtered) < len(times[data.name]):
-
 		filtered = np.append(filtered, filtered[-1])
+
 	try:
 		sens_data[data.name] += filtered[-1] * (times[data.name][-1] - times[data.name][-2])
+
 	except:
 		pass
 
@@ -54,14 +53,12 @@ def callback(data):
 
 #standard ros subscriber
 def listener():
-
 	rospy.init_node("integrator")
 	rospy.Subscriber("sensor_rates", transfer_rate, callback, queue_size=1)
 
 if __name__ == '__main__':
-	
 	pub = rospy.Publisher("sensor_data", sent_data, queue_size=1)
-	
+
 	while not rospy.is_shutdown():
 
 		listener()
